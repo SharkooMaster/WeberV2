f470748f9e8b0c007f43526d982dda9539d10b1d
Ali Al Rashini <44705253+SharkooMaster@users.noreply.github.com>
2023-03-26 05:49:15 +0200

    init compiler

--- weberCompiler.py
+++ weberCompiler.py
@@ -3,5 +3,18 @@ from weberParser import parser
 from weberToken import token
 
 class compiler:
-    def __init__(self):
-        print("init compiler")
+	def __init__(self):
+		print("init compiler")
+		self.grammer = [
+			["LE"],
+			
+			["global", "STRING", "EQ", "STRING", "SC"],
+			["global", "STRING", "EQ", "STRING", "LE"],
+			["global", "STRING", "EQ", "STR", "STRING", "STR", "SC"],
+			["global", "STRING", "EQ", "STR", "STRING", "STR", "LE"],
+			
+			["local", "STRING", "EQ", "STRING", "SC"],
+			["local", "STRING", "EQ", "STRING", "LE"],
+			["local", "STRING", "EQ", "STR", "STRING", "STR", "SC"],
+			["local", "STRING", "EQ", "STR", "STRING", "STR", "LE"]
+		]
